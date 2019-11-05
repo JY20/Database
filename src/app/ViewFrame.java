0a519a40c30be4c22872ba96510bdff4cc3a5516
JY20 <jimmyyan1337@gmail.com>
2019-11-05 14:40:29 -0500

    Turn data into jlabel

--- src/app/ViewFrame.java
+++ src/app/ViewFrame.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class ViewFrame extends JFrame{
 	
@@ -16,16 +18,34 @@ public class ViewFrame extends JFrame{
 
 	public ViewFrame (String title){
 		super(title);
-		ArrayList<String>[] data;
+		ArrayList<String>[] data = DatabaseGUI.database.getDatabase();
 		JButton buttonBack = new JButton("Back");
+		JButton buttonNextP= new JButton("Next Page");
+		int size = 10;
+		JLabel[] dataL = new JLabel[size];
+		int page = 1;
 
 		//ata = DatabaseGUI.database.database;
-	    GridLayout viewgrid = new GridLayout(5,5, 30, 10);
+	    GridLayout viewgrid = new GridLayout(size,7, 30, 1);
 	    setLayout(viewgrid);
-	        
 	    Container c = getContentPane();
-		c.add(buttonBack);
+	    c.add(buttonBack);
+	    for (int i = 0; i < page; i++) {
+			for(int j = 0; j < 7; j++) {
+				dataL[j] = new JLabel("tw",SwingConstants.CENTER);
+				c.add(dataL[j]);
+			}
+		}
+		c.add(buttonNextP);
+		
 		buttonBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.viewFrame.setVisible(false);
+                DatabaseGUI.frame.setVisible(true);
+            }
+        });
+		buttonNextP.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 MainFrame.viewFrame.setVisible(false);
