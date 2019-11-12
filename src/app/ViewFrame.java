524ddc54834df631461da60534a1f6d93eb98893
JY20 <jimmyyan1337@gmail.com>
2019-11-12 14:00:58 -0500

    Testing gridlayout on the view frame

--- src/app/ViewFrame.java
+++ src/app/ViewFrame.java
@@ -1,21 +1,20 @@
 package app;
 
 import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
 public class ViewFrame extends JFrame {
@@ -26,12 +25,9 @@ public class ViewFrame extends JFrame {
 		super(title);
 		JPanel panelInfo = new JPanel();
 		JButton buttonBack = new JButton("Back");
-		int size = 10;
 		ArrayList <String>dataAll = new ArrayList<>();
 		String[] data = new String []{"ID", "Name", "Age", "Period 1", "Period 2", "Period 3", "Period 4", "Period 5"};
-		JLabel[][] dataL;
-		JLabel [] L = null;
-	    JTextArea textArea = new JTextArea(20, 20);  
+		JLabel[][] dataL; 
 	    JScrollPane sTextArea;
 	    
 		try {
@@ -47,7 +43,7 @@ public class ViewFrame extends JFrame {
 		}
 		dataL = new JLabel[dataAll.size()+1][8];
 		
-	    GridLayout viewgrid = new GridLayout(dataAll.size()+2, 8, 10, 1);
+	    GridLayout viewgrid = new GridLayout(dataAll.size()+2, 8, 1, 1);
 	    panelInfo.setLayout(viewgrid);
 	    
 	    Container c = getContentPane();
@@ -76,7 +72,22 @@ public class ViewFrame extends JFrame {
 	    }
 	    sTextArea = new JScrollPane(panelInfo); 
 	    sTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); 
-	    c.add(sTextArea);
+	    JPanel panel = new JPanel();
+	    panel.setLayout(new GridBagLayout());
+	    GridBagConstraints GC = new GridBagConstraints();
+	    GC.fill = GridBagConstraints.HORIZONTAL;
+	    GC.ipady = 0;      //make this component tall
+	    GC.weightx = 0.0;
+	    GC.gridwidth = 3;
+	    GC.gridx = 0;
+	    GC.gridy = 0;
+	    panel.add(buttonBack, GC);
+	    
+	    /*GC.gridx = 0;
+	    GC.gridy = 400;
+	    panel.add(buttonBack, GC);*/
+
+	    c.add(panel);
 	    
 		buttonBack.addActionListener(new ActionListener() {
             @Override
@@ -86,5 +97,4 @@ public class ViewFrame extends JFrame {
             }
         });
 	}
-
 }
