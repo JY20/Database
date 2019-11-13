4a361de7278de7925e8acab2b61441d5882e7c2b
JY20 <jimmyyan1337@gmail.com>
2019-11-12 21:20:36 -0500

    Back button working in view frame

--- src/app/ViewFrame.java
+++ src/app/ViewFrame.java
@@ -1,8 +1,6 @@
 package app;
 
 import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,16 +18,29 @@ import javax.swing.SwingConstants;
 public class ViewFrame extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-	
+	static JPanel panelInfo = new JPanel();
+	static JButton buttonBack = new JButton("Back");
+	static ArrayList <String>dataAll = new ArrayList<>();
+	static String[] data = new String []{"ID", "Name", "Age", "Period 1", "Period 2", "Period 3", "Period 4", "Period 5"};
+	static JLabel[][] dataL; 
+    static JScrollPane sTextArea;
+    static Container c;
+    
 	public ViewFrame (String title) {
 		super(title);
-		JPanel panelInfo = new JPanel();
-		JButton buttonBack = new JButton("Back");
-		ArrayList <String>dataAll = new ArrayList<>();
-		String[] data = new String []{"ID", "Name", "Age", "Period 1", "Period 2", "Period 3", "Period 4", "Period 5"};
-		JLabel[][] dataL; 
-	    JScrollPane sTextArea;
-	    
+		
+		c = getContentPane();
+		refresh();
+		buttonBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.viewFrame.setVisible(false);
+                DatabaseGUI.frame.setVisible(true);
+            }
+        });
+	}
+	
+	public static void refresh() {
 		try {
 			FileReader viewR = new FileReader("Database");
 			BufferedReader viewFile = new BufferedReader(viewR);
@@ -42,11 +53,9 @@ public class ViewFrame extends JFrame {
 		} catch(IOException io) {		
 		}
 		dataL = new JLabel[dataAll.size()+1][8];
-		
 	    GridLayout viewgrid = new GridLayout(dataAll.size()+2, 8, 1, 1);
 	    panelInfo.setLayout(viewgrid);
 	    
-	    Container c = getContentPane();
 	    for (int i = 0; i < dataAll.size(); i++) {
 	    	if (i == 0) {
 	    	}else {
@@ -62,39 +71,11 @@ public class ViewFrame extends JFrame {
 	    		panelInfo.add(dataL[i][j]);
 	    	}
 		}
-	    for (int i = 0; i < 8; i++) {
-	    	if(i == 1) {
-	    		panelInfo.add(buttonBack);
-	    	}else {
-	    		dataL[0][0] = new JLabel("",SwingConstants.CENTER);
-	    		panelInfo.add(dataL[0][0]);
-	    	}
-	    }
+	    
 	    sTextArea = new JScrollPane(panelInfo); 
-	    sTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); 
-	    JPanel panel = new JPanel();
-	    panel.setLayout(new GridBagLayout());
-	    GridBagConstraints GC = new GridBagConstraints();
-	    GC.fill = GridBagConstraints.HORIZONTAL;
-	    GC.ipady = 0;      //make this component tall
-	    GC.weightx = 0.0;
-	    GC.gridwidth = 3;
-	    GC.gridx = 0;
-	    GC.gridy = 0;
-	    panel.add(buttonBack, GC);
-	    
-	    /*GC.gridx = 0;
-	    GC.gridy = 400;
-	    panel.add(buttonBack, GC);*/
-
-	    c.add(panel);
-	    
-		buttonBack.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                MainFrame.viewFrame.setVisible(false);
-                DatabaseGUI.frame.setVisible(true);
-            }
-        });
+	    sTextArea.setCorner(JScrollPane.UPPER_RIGHT_CORNER, buttonBack);
+	    sTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        sTextArea.setColumnHeaderView(buttonBack);
+	    c.add(sTextArea);
 	}
 }
