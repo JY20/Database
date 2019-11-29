c87de6548deee61f4e0acb9a78b02d069d7e086e
Haashim Rehan <haashimr@hotmail.com>
2019-11-29 14:19:17 -0500

    Commented DatabaseGUI

--- src/app/DatabaseGUI.java
+++ src/app/DatabaseGUI.java
@@ -1,5 +1,6 @@
 package app;
 // Jimmy Yan and Haashim Rehan
+
 // 10 29 2019
 
 // Import the libraries
@@ -17,9 +18,9 @@ public class DatabaseGUI {
     public static String[] questions = new String[] { "ID", "Name", "Age", "Period 1 Class", "Period 2 Class",
             "Period 3 Class", "Period 4 Class", "Period 5 Class" }; // questions
 
-    static Database database = new Database(questions); // 
-    
-    static JFrame frame = new MainFrame("Database"); 
+    static Database database = new Database(questions); // Creates a database object with the questions as columns
+
+    static JFrame frame = new MainFrame("Database"); // Menu screen
 
     public static void main(String[] args) throws IOException {
 
@@ -28,9 +29,9 @@ public class DatabaseGUI {
             public void run() {
 
                 frame.setSize(600, 500);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit program when frame is closed
+                frame.setLocationRelativeTo(null); // start frame in center
+                frame.setVisible(true); // make frame visible
             }
         });
     }
