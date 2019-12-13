4000ebb02b34762443240e0691187c7eb51088ad
Haashim Rehan <haashimr@hotmail.com>
2019-12-13 14:08:13 -0500

    Fixed the delete Data

--- src/app/DeleteFrame.java
+++ src/app/DeleteFrame.java
@@ -19,48 +19,53 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class DeleteFrame  extends JFrame {
+public class DeleteFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public static int ylength = 500; // The length of the frame
-	public static ArrayList<String> data = new ArrayList <>(); // The data from the database
-	
+	public static ArrayList<String> data = new ArrayList<>(); // The data from the database
+	public static String[] emptyData = new String[DatabaseGUI.newInfo.length];
+
 	public DeleteFrame(String title) {
 		super(title);
-	
-		JButton buttonDelete = new JButton("Delete"); //Button to delete the data
+
+		for (int i = 0; i < emptyData.length; i++) {
+			emptyData[i] = "*";
+		}
+
+		JButton buttonDelete = new JButton("Delete"); // Button to delete the data
 		JButton buttonBack = new JButton("Back");// Button to go back to the main frame
-		JTextField text = new JTextField ("ID"); // The area where the user enter the id 
-		text.setFont(new Font (Font.DIALOG,Font.BOLD,15) );//Change the font
+		JTextField text = new JTextField("ID"); // The area where the user enter the id
+		text.setFont(new Font(Font.DIALOG, Font.BOLD, 15));// Change the font
 		Container c = getContentPane();// Declare the container
-		JPanel Bpanel = new JPanel ();// The panel for the buttons and text box
-		
-		Bpanel.setLayout(new GridBagLayout()); //Using bag grid layout to make different grid size
-		GridBagConstraints overall  = new GridBagConstraints();
-		
-		overall .fill = GridBagConstraints.HORIZONTAL; // Add the back button
+		JPanel Bpanel = new JPanel();// The panel for the buttons and text box
+
+		Bpanel.setLayout(new GridBagLayout()); // Using bag grid layout to make different grid size
+		GridBagConstraints overall = new GridBagConstraints();
+
+		overall.fill = GridBagConstraints.HORIZONTAL; // Add the back button
 		overall.weightx = 0.5;
-		overall .ipady = (int) (ylength*0.3);  
-		overall .gridx = 0;
-		overall .gridy = 0;
+		overall.ipady = (int) (ylength * 0.3);
+		overall.gridx = 0;
+		overall.gridy = 0;
 		Bpanel.add(buttonBack, overall);
 
-		overall .fill = GridBagConstraints.HORIZONTAL; // Add the delete button
-		overall .weightx = 0.5;
-		overall .ipady = (int) (ylength*0.3);  
-		overall .gridx = 1;
-		overall .gridy = 0;
-		Bpanel.add(buttonDelete, overall );
+		overall.fill = GridBagConstraints.HORIZONTAL; // Add the delete button
+		overall.weightx = 0.5;
+		overall.ipady = (int) (ylength * 0.3);
+		overall.gridx = 1;
+		overall.gridy = 0;
+		Bpanel.add(buttonDelete, overall);
 
-		overall .fill = GridBagConstraints.HORIZONTAL; // Add the text box  
-		overall .ipady = (int) (ylength*0.7);
-		overall .weightx = 0.0;
-		overall .gridwidth = 2;
-		overall .gridx = 0;
-		overall .gridy = 1;
+		overall.fill = GridBagConstraints.HORIZONTAL; // Add the text box
+		overall.ipady = (int) (ylength * 0.7);
+		overall.weightx = 0.0;
+		overall.gridwidth = 2;
+		overall.gridx = 0;
+		overall.gridy = 1;
 		Bpanel.add(text, overall);
-		
+
 		c.add(Bpanel); // Add all the components to the frame
-		
+
 		buttonBack.addActionListener(new ActionListener() { // When back button is pressed it returns to main frame
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -68,23 +73,23 @@ public class DeleteFrame  extends JFrame {
 				DatabaseGUI.frame.setVisible(true);
 			}
 		});
-		
+
 		buttonDelete.addActionListener(new ActionListener() { // When delete button is pressed it deletes the data
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				try {
 					String input = text.getText();
 					read();
-					if (input.length() <= 5 ) {
+					if (input.length() <= 5) {
 						try {
 							int tempInt = Integer.parseInt(input);
 							if (search(tempInt) == true) {
 								write();
 								MainFrame.deleteFrame.setVisible(false);
 								DatabaseGUI.frame.setVisible(true);
-							} 
-						} catch(Exception E) {
+							}
+						} catch (Exception E) {
 							text.setText("Input not vaild please try again");
 						}
 					}
@@ -93,53 +98,57 @@ public class DeleteFrame  extends JFrame {
 				}
 			}
 		});
-		
+
 	}
-	
-	public static void read () throws IOException { // Read all the data from the file
-        FileReader fr = new FileReader("Database");
+
+	public static void read() throws IOException { // Read all the data from the file
+		FileReader fr = new FileReader("Database");
 		BufferedReader BrDF = new BufferedReader(fr);
-		data = new ArrayList <>();
-        String temps = BrDF.readLine();
-        while(temps != null) {
-        	data.add(temps);
-        	temps = BrDF.readLine();
-        }
-        BrDF.close();
+		data = new ArrayList<>();
+		String temps = BrDF.readLine();
+		while (temps != null) {
+			data.add(temps);
+			temps = BrDF.readLine();
+		}
+		BrDF.close();
 	}
-	
-	public static boolean search (int id) { // Search through the data to make sure the user inputs in a valid id
+
+	public static boolean search(int id) { // Search through the data to make sure the user inputs in a valid id
 		String temps;
-        for(int i = 0; i < data.size(); i++) {
-        	temps = data.get(i).substring(0, 5);
-        	int tempInt = Integer.parseInt(temps);
-        	if (tempInt == id) {
-        		temps = Integer.toString(id);
-        		int digits = 5-temps.length();
-        		for(int k =  0;  k < digits; k ++) {
-        			temps = "0"+ temps;
-        		}
-        		for(int k =  0;  k < 12; k ++) {
-        			temps = temps + ",*";
-        		}
-        		data.set(i, temps + ",");
-        		i = data.size();
-        		return true;
-        	}
-        }
-        return false;
+
+		DatabaseGUI.database.updateRow(id, emptyData);
+		for (int i = 0; i < data.size(); i++) {
+			temps = data.get(i).substring(0, 5);
+			int tempInt = Integer.parseInt(temps);
+			if (tempInt == id) {
+				temps = Integer.toString(id);
+				int digits = 5 - temps.length();
+				for (int k = 0; k < digits; k++) {
+					temps = "0" + temps;
+				}
+				for (int k = 0; k < 12; k++) {
+					temps = temps + ",*";
+				}
+				data.set(i, temps + ",");
+				i = data.size();
+
+				return true;
+			}
+		}
+		return false;
 	}
-	
-	public static void write () throws IOException { // Rewrite the the new data with the delete data
-        FileWriter fw = new FileWriter("Database", false);
-        BufferedWriter BwDF = new BufferedWriter(fw);
-        for(int i = 0; i < data.size(); i++) {
-        	if (i == (data.size()-1)) {
-        		BwDF.write(data.get(i));
-        	}else {
-        		BwDF.write(data.get(i) + "\n");
-        	}
-        }
-        BwDF.close();
+
+	public static void write() throws IOException { // Rewrite the the new data with the delete data
+		FileWriter fw = new FileWriter("Database", false);
+		BufferedWriter BwDF = new BufferedWriter(fw);
+		for (int i = 0; i < data.size(); i++) {
+			if (i == (data.size() - 1)) {
+				BwDF.write(data.get(i));
+			} else {
+				BwDF.write(data.get(i) + "\n");
+			}
+		}
+		BwDF.close();
+
 	}
 }
